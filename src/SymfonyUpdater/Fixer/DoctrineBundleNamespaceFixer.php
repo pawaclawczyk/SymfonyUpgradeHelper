43fd9258d48d62d269e023f911077b1e0e967eaf
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2014-02-25 23:59:43 +0100

    Make Doctrine namespace fixer more common.

--- src/SymfonyUpdater/Fixer/DoctrineBundleNamespaceFixer.php
+++ src/SymfonyUpdater/Fixer/DoctrineBundleNamespaceFixer.php
@@ -8,7 +8,7 @@ class DoctrineBundleNamespaceFixer implements Fixer
 {
     public function fix(\SplFileInfo $file, $content)
     {
-        return preg_replace('/(Symfony\\\Bundle\\\DoctrineBundle\\\DoctrineBundle)/', 'Doctrine\Bundle\DoctrineBundle\DoctrineBundle', $content);
+        return preg_replace('/(Symfony\\\Bundle\\\DoctrineBundle)/', 'Doctrine\Bundle\DoctrineBundle', $content);
     }
 
     public function getName()
